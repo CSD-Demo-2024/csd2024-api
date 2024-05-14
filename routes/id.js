bda5fb315225f4809355a2047ad20d3185cd50ad
Roman Braunstingl <r.braunstingl@celix.at>
2024-05-14 11:09:16 +0200

    implement id gen API

--- routes/id.js
+++ routes/id.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/generate', function(req, res, next) {
+  import('nanoid').then(({nanoid}) => res.send(nanoid()));
 });
 
 module.exports = router;
